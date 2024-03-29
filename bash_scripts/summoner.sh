ba5c158a338f78ae9f09db7020f5402279abe7c2
Jacob Anderson <jakejoe97@gmail.com>
2024-03-29 07:55:12 -0600

    updated file to use conda

--- bash_scripts/summoner.sh
+++ bash_scripts/summoner.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+conda activate riot
+
 python_scripts=("../summoners/br1.py" "../summoners/eun1.py" "../summoners/euw1.py" "../summoners/jp1.py" "../summoners/kr.py" "../summoners/la1.py" "../summoners/la2.py" "../summoners/na1.py" "../summoners/oc1.py" "../summoners/ph2.py" "../summoners/ru.py" "../summoners/sg2.py" "../summoners/th2.py" "../summoners/tr1.py" "../summoners/tw2.py" "../summoners/vn2.py")
 
 for script in "${python_scripts[@]}"
